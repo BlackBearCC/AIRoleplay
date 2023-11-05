472b6f2701d8f1f001087deb2268549cc62d5d22
leozy <947674861@qq.com>
2023-11-05 22:49:56 +0800

    langchain跑通

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import llm_selector
 import charact_selector
 import prompt_manages
+from langchain.prompts import ChatPromptTemplate
 
 
 minimax = llm_selector.initialize_minimax()
 
 tuji = charact_selector.initialize_tuji()
 
-#初始化prompt
-prompt = prompt_manages.initialize_prompt()
-print(prompt)
+prompt = prompt_manages.rolePlay()+tuji+prompt_manages.charactorInfo()+prompt_manages.plotDevelopment()
+final_prompt = ChatPromptTemplate.from_template(prompt)
+
+chain = final_prompt | minimax
+
+for chunk in chain.stream({"user":"大头","char":"兔叽","input":"你是谁"}):
+    print(chunk)
