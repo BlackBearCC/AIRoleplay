cf453d2c11b4aee625ba04ac5c99742e8aa0bf2c
leozy <947674861@qq.com>
2023-11-05 22:35:01 +0800

    基础prompt初始化

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import llm_selector
 import charact_selector
+import prompt_manages
 
 
 minimax = llm_selector.initialize_minimax()
 
 tuji = charact_selector.initialize_tuji()
 
+#初始化prompt
+prompt = prompt_manages.initialize_prompt()
+print(prompt)
