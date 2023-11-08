1417f99dd46684f3c319c1284eaa7a29a47e7141
leozy <947674861@qq.com>
2023-11-08 22:49:35 +0800

    添加汤师爷

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import prompt_manages
 from langchain.prompts import ChatPromptTemplate
 from langchain.callbacks import get_openai_callback
 from langchain.embeddings import HuggingFaceBgeEmbeddings
+
+from langchain.document_loaders import JSONLoader
+import json
+from pathlib import Path
+from pprint import pprint
+
 from langchain.document_loaders.csv_loader import CSVLoader
 
 minimax = llm_selector.initialize_minimax()
@@ -14,21 +20,26 @@ tuji = charact_selector.initialize_tuji()
 prompt = prompt_manages.rolePlay()+tuji+prompt_manages.charactorStyle()+prompt_manages.plotDevelopment()
 final_prompt = ChatPromptTemplate.from_template(prompt)
 
+# file_path='tangshiye_test_output_dialogue.jsonl'
+loader = CSVLoader(file_path=r'tangshiye_test_output_dialogue.csv')
+data = loader.load()
+# data = json.loads(Path(file_path).read_text())
+print(data)
 # chain = final_prompt | minimax
-chain = final_prompt | qianfan
-
-
-user_input = input()
-
-model_name = "BAAI/bge-large-en-v1.5"
-model_kwargs = {'device': 'cuda'}
-encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
-hf = HuggingFaceBgeEmbeddings(
-    model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
-)
-embedding = hf.embed_query("hi this is harrison")
-len(embedding)
-print(embedding)
+# chain = final_prompt | qianfan
+#
+#
+# user_input = input()
+#
+# model_name = "BAAI/bge-large-en-v1.5"
+# model_kwargs = {'device': 'cuda'}
+# encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
+# hf = HuggingFaceBgeEmbeddings(
+#     model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+# )
+# embedding = hf.embed_query("hi this is harrison")
+# len(embedding)
+# print(embedding)
 
 # with get_openai_callback() as callback:
 #     for chunk in chain.stream({"user": "大头", "char": "兔叽", "input": user_input}):
